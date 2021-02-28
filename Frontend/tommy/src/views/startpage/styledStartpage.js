fb8b61a9ee41c601843a4679c63afe40c9925954
Tommy <tommynguyen@live.se>
2021-02-28 03:47:11 +0100

    New update

---
+++ Frontend/tommy/src/views/startpage/styledStartpage.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StartContainer = styled.div`
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+export const NavBar = styled.div`
+  height: 500px;
+  width: 500px;
+`;
